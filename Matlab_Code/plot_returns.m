e3911935f54960ea80bdfa460783e45cc13152b7
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-05-28 16:27:07 +0200

    Update plot_returns.m

--- Matlab_Code/plot_returns.m
+++ Matlab_Code/plot_returns.m
@@ -1,23 +1,23 @@
-function plot_returns(Returns)
+function plot_returns(Market_EU, Market_US, Returns)
 % This function plots the returns of the EURO STOXX50 and S&P500 indexes
 %
 % INPUT
 % Returns: struct containing the daily and yearly returns of the EURO STOXX50 and S&P500 indexes
 
 % Yearly returns linspace
-x_yearly = linspace(1,length(Returns.Daily(:,1)),length(Returns.Annually(:,1)));
+x_yearly = linspace(1,length(Returns.Daily(:,1)),length(Returns.Annually(:,1))+1);
 % Daily returns linspace
-x_daily = 1:length(Returns.Daily(:,1));
+x_daily = 1:length(Returns.Daily(:,1))+1;
 
 % Plot the returns
 figure;
 % Plot the yearly returns EU and US
-plot(x_yearly,flip(EU_Market.spot.*ret2price(Returns.Returns.Annually(:,2))),  '--', 'Color', [0.3 0.75 0.93], 'LineWidth', 2);
+plot(x_yearly,flip(Market_EU.spot.*ret2price(Returns.Annually(:,2))),  '--', 'Color', [0.3 0.75 0.93], 'LineWidth', 2);
 hold on;
-plot(x_yearly,flip(US_Market.spot.*ret2price(Returns.Returns.Annually(:,1))), '--', 'Color', [1 0.6 0.78], 'LineWidth', 2);
+plot(x_yearly,flip(Market_US.spot.*ret2price(Returns.Annually(:,1))), '--', 'Color', [1 0.6 0.78], 'LineWidth', 2);
 % Plot the daily returns EU and US
-plot(x_daily,flip(EU_Market.spot.*ret2price(Returns.Returns.Daily(:,2))), 'b', 'LineWidth', 2);
-plot(x_daily,flip(US_Market.spot.*ret2price(Returns.Returns.Daily(:,1))), 'r', 'LineWidth', 2); 
+plot(x_daily,flip(Market_EU.spot.*ret2price(Returns.Daily(:,2))), 'b', 'LineWidth', 2);
+plot(x_daily,flip(Market_US.spot.*ret2price(Returns.Daily(:,1))), 'r', 'LineWidth', 2); 
 
 % Improve the plot
 title('Returns trend', 'FontSize', 14);
