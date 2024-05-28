ba91033d6e13fb53353df523acf551c89b83526e
7ranc35c0 <167651768+7ranc35c0@users.noreply.github.com>
2024-05-28 16:27:00 +0200

    Update constraint.m

--- Matlab_Code/Calibration/constraint.m
+++ Matlab_Code/Calibration/constraint.m
@@ -1,16 +1,31 @@
 function [c, ceq] = constraint(x, alpha)
-    % function that represents the constraint in the Least Squares Calibration
-    %p1=sigma
-    %p2 k, p3 theta
-    
-    %c= [- 0.5*x(2)*x(1)^2;
-    %    - 0.5 * x(5) * x(4)^2;];
-    c = [];
+% This is the constraint function for the "fmincon" Calibration. 
+%
+% INPUTS
+% x: vector of parameters [sigma, kappa, theta]
+% alpha: exponent of the model
+%
+% OUTPUTS
+% c: inequality constraint
+% ceq: equality constraint
 
-%        c= [- 0.5*x(2)*x(1)^2;
-%         - 0.5 * x(5) * x(4)^2;
-%         - (1-alpha) / (x(2)*x(1)^2) - x(3);
-%         - (1-alpha) / (x(5)*x(4)^2) - x(6);];
-    
-    ceq = x(1)^2/(x(3)^2*x(2)) - x(4)^2/(x(6)^2*x(5));
+% x(1) = sigma
+% x(2) = kappa
+% x(3) = theta
+
+% Inequality constraint
+
+% c= [- 0.5*x(2)*x(1)^2;
+%     - 0.5*x(5)*x(4)^2 ];
+
+% c= [- 0.5*x(2)*x(1)^2;
+%     - 0.5 * x(5) * x(4)^2;
+%     - (1-alpha) / (x(2)*x(1)^2) - x(3);
+%     - (1-alpha) / (x(5)*x(4)^2) - x(6) ];
+
+c = [];
+
+% Equality constraint
+ceq = x(1)^2/(x(3)^2*x(2)) - x(4)^2/(x(6)^2*x(5));
+
 end
