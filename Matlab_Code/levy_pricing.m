1cc5bcefeff1390db3fe0da1c65963537e334ea2
7ranc35c0 <167651768+7ranc35c0@users.noreply.github.com>
2024-05-30 18:01:36 +0200

    Update levy_pricing.m

--- Matlab_Code/levy_pricing.m
+++ Matlab_Code/levy_pricing.m
@@ -1,38 +1,38 @@
-function derivativePrice_MC = levy_pricing(Market_EU, S0_US, settlement, targetDate, ...
-                                alpha, kappa_EU, kappa_US, sigma_EU, sigma_US, theta_EU, theta_US, nSim)
+function price = levy_prcinig(Market_US, S0_EU, settlement, targetDate, ...
+                                alpha, kappa_US, kappa_EU, sigma_US, sigma_EU, theta_US, theta_EU, N_sim)
 
 % This function computes the price of a derivative using a Monte Carlo simulation under Lévy processes
 % 
 % INPUTS
-%  Market_EU: structure containing the market data for the European market
-%  S0_US: spot price of the US market
+%  Market_US: structure containing the market data for the US market
+%  S0_EU: spot price of the European market
 %  settlement: settlement date
 %  targetDate: target date
 %  alpha: Lévy exponent
-%  kappa_EU: Lévy parameter for the EU market
 %  kappa_US: Lévy parameter for the US market
-%  sigma_EU: volatility for the EU market
+%  kappa_EU: Lévy parameter for the EU market
 %  sigma_US: volatility for the US market
-%  theta_EU: drift for the EU market
+%  sigma_EU: volatility for the EU market
 %  theta_US: drift for the US market
-%  nSim: number of simulations
+%  theta_EU: drift for the EU market
+%  N_sim: number of simulations
 %
 % OUTPUT
-%  derivativePrice_MC: price of the derivative
+%  price: price of the derivative
 
-S0_EU = Market_EU.spot;
+S0_US = Market_US.spot;
 % Simulation of the NIG processes
-% use a MonteCarlo simulation to compute the call prices
-Expiries = datenum([Market_EU.datesExpiry]');
+% Use a Montecarlo simulation to compute the call prices
+Expiries = datenum([Market_US-datesExpiry]');
 
-B_bar = [Market_EU.B_bar.value]';
+B_bar = [Market_US.B_bar.value]';
 
 % Compute the discount
 discount = intExtDF(B_bar, Expiries, targetDate);
 
 % Compute the forward prices
-F0_EU = S0_EU/discount;
-F0_US = S0_US/discount; % dscount sbagliato
+F0_US = S0_US/discount;
+F0_EU = S0_EU/discount; % dscount sbagliato
 
 % più o meno giusti ma come li otteniamo?????????
 F0_EU = 4345;
@@ -43,18 +43,19 @@ ACT_365 = 3;
 ttm = yearfrac(settlement, targetDate, ACT_365);
 
 % compute the Laplace exponent EU & US
-ln_L_EU = @(omega_EU) ttm/kappa_EU * (1 - alpha)/alpha * ...
-    (1 - (1 + (omega_EU .* kappa_EU * sigma_EU^2)/(1-alpha)).^alpha );
-
 ln_L_US = @(omega_US) ttm/kappa_US * (1 - alpha)/alpha * ...
     (1 - (1 + (omega_US .* kappa_US * sigma_US^2)/(1-alpha)).^alpha );
 
+ln_L_EU = @(omega_EU) ttm/kappa_EU * (1 - alpha)/alpha * ...
+    (1 - (1 + (omega_EU .* kappa_EU * sigma_EU^2)/(1-alpha)).^alpha );
+    
+
 % draw the standard normal random variables
-g = mvnrnd([0; 0], [1 0.801; 0.801 1], nSim);
-% g = randn(nSim, 1);
+g = mvnrnd([0; 0], [1 0.801; 0.801 1], N_sim);
+% g = randn(N_sim, 1);
 % draw the inverse gaussian random variables
-G_US = random('inversegaussian', 1, ttm/kappa_US, nSim, 1);
-G_EU = random('inversegaussian', 1, ttm/kappa_EU, nSim, 1);
+G_EU = random('inversegaussian', 1, ttm/kappa_EU, N_sim, 1);
+G_US = random('inversegaussian', 1, ttm/kappa_US, N_sim, 1);
 
 % ft_EU = sqrt(ttm) * sigma_EU * sqrt(G) .* g(:,1) - (0.5 + theta_EU) * ttm * sigma_EU^2 * G - ln_L_EU(theta_EU);
 % ft_US = sqrt(ttm) * sigma_US * sqrt(G) .* g(:,2) - (0.5 + theta_US) * ttm * sigma_US^2 * G - ln_L_US(theta_US);
@@ -77,27 +78,22 @@ G_EU = random('inversegaussian', 1, ttm/kappa_EU, nSim, 1);
 % ft_US = sqrt(ttm) * sigma_US * sqrt(G_US) .* g(:,2) - (0.5 + theta_US) * ttm * sigma_US^2 * G_US - ln_L_US(theta_US)...
 %             - ttm./kappa_US * (1-sqrt(1-2.*kappa_US.*theta_US - kappa_US.*sigma_US .^2));
 
-ft_EU = sqrt(ttm) * sigma_EU * sqrt(G_EU) .* g(:,1) - (0.5 + theta_EU) * ttm * sigma_EU^2 * G_EU - ln_L_EU(theta_EU)...
-                + ttm./kappa_EU * ( sqrt(1 - 2*kappa_EU) - 1 );
-
 ft_US = sqrt(ttm) * sigma_US * sqrt(G_US) .* g(:,2) - (0.5 + theta_US) * ttm * sigma_US^2 * G_US - ln_L_US(theta_US)...
                 + ttm./kappa_US * ( sqrt(1 - 2*kappa_US) - 1 );
 
-S1_EU = F0_EU * exp(ft_EU); 
+ft_EU = sqrt(ttm) * sigma_EU * sqrt(G_EU) .* g(:,1) - (0.5 + theta_EU) * ttm * sigma_EU^2 * G_EU - ln_L_EU(theta_EU)...
+                + ttm./kappa_EU * ( sqrt(1 - 2*kappa_EU) - 1 );
 
 S1_US = F0_US * exp(ft_US); 
 
-% indicator function US
-Indicator_function = (S1_US < 0.95 * S0_US);
+S1_EU = F0_EU * exp(ft_EU); 
 
-% check
-prova = S1_EU - S0_EU;
-maggioridi0 = sum(prova>0);
-maggioridi02 = sum(Indicator_function);
-prova2 = max(S1_EU - S0_EU, 0);
+
+% indicator function US
+Indicator_function = (S1_EU < 0.95 * S0_EU);
 
 % Compute the payoff
-payoff = max(S1_EU - S0_EU, 0) .* Indicator_function;
+payoff = max(S1_US - S0_US, 0) .* Indicator_function;
 
 % Compute the price
 derivativePrice_MC = discount * mean(payoff);
