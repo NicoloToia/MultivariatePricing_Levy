b1fcc4d7228d3be05767f4d3b9ad6c20f3ef3127
NickToi <160607944+NicoloToia@users.noreply.github.com>
2024-06-02 15:46:05 +0200

    Create cosnt_Nu.m

---
+++ Matlab_Code/cosnt_Nu.m
@@ -0,0 +1,33 @@
+function [c, ceq] = cosnt_Nu(nus, kappa_EU,kappa_US)
+% This function is used to impose the constraints on the optimization
+% problem. The constraints are the following:
+% 1. kappa_EU - (nus(1)*nus(3))/(nus(1)+nus(3)) = 0
+% 2. kappa_US - (nus(2)*nus(3))/(nus(2)+nus(3)) = 0
+% 3. sqrt((nus(1) * nus(2)) / ((nus(1) + nus(3)) * (nus(2) + nus(3)))) - (sqrt(kappa_EU * kappa_US)/ nus(3)) = 0
+%
+% INPUTS:
+% nus: vector of the parameters to be optimized
+% kappa_EU: the kappa parameter for the EU market
+% kappa_US: the kappa parameter for the US market
+%
+% OUTPUTS:
+% c: inequality constraints
+% ceq: equality constraints
+
+% nu_US = nus(1);
+% nu_EU = nus(2);
+% nu_Z = nus(3);
+
+% Inequality constraints
+c = [];
+
+% Equality constraints
+% ceq = [ kappa_US - nus(1)*nus(3)/(nus(1)+nus(3));
+%         kappa_EU - nus(2)*nus(3)/(nus(2)+nus(3));
+%         sqrt((nus(1) * nus(2)) / ((nus(1) + nus(3)) * (nus(2) + nus(3)))) - (sqrt(kappa_EU * kappa_US)/ nus(3))
+%         ];
+
+ceq = [ kappa_US - (nus(1)*nus(3))/(nus(1)+nus(3));
+        kappa_EU - (nus(2)*nus(3))/(nus(2)+nus(3))
+        ];
+end
