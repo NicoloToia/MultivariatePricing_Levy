b140a7b12c074ca9e7ab22fbbce12c2670b91b99
giacomomanfredi2001 <giacomo2.manfredi@mail.polimi.it>
2024-06-05 19:10:42 +0200

    plot3d prova eu

---
+++ Matlab_Code/plot3d_impl_vol_new.m
@@ -0,0 +1,99 @@
+function plot3d_impl_vol_new(Filtered_Market)
+% 3D plot
+
+% Step 1: Set the grid
+
+% find the boundaries for the y
+y_min = min(Filtered_Market.strikes(1).value);
+y_max = max(Filtered_Market.strikes(1).value);
+for ii = 1 : length(Filtered_Market.datesExpiry)
+    y_min = min([Filtered_Market.strikes(ii).value, y_min]);
+    y_max = max([Filtered_Market.strikes(ii).value, y_max]);
+end
+
+[xGrid, yGrid, zGrid] = deal([]);
+
+for ii = 1:length(Filtered_Market.datesExpiry)
+    expiry = Filtered_Market.datesExpiry(ii);
+    strikes = Filtered_Market.strikes(ii).value;
+    impliedVols = Filtered_Market.OTM_ImpVol(ii).value;
+    
+    % Espandi le griglie con i nuovi dati
+
+    [x, y] = meshgrid(expiry, strikes);
+    
+    xGrid = [xGrid; x(:)];
+    yGrid = [yGrid; y(:)];
+    zGrid = [zGrid; impliedVols(:)];
+end
+xGrid;
+zGrid;
+% Step 2: Crete the plot
+figure;
+scatter3(xGrid, yGrid, zGrid);
+xlabel('Expiries');
+ylabel('Strikes');
+zlabel('Implied Volatilities');
+title('3D Scatter Plot of Implied Volatilities');
+grid on;
+
+%% Surface
+
+% find the boundaries for the y and the smallest step 
+y_min = min(Filtered_Market.strikes(1).value);
+y_max = max(Filtered_Market.strikes(1).value);
+step_min = min(Filtered_Market.strikes(1).value(2:end)-Filtered_Market.strikes(1).value(1:end-1));
+for ii = 1 : length(Filtered_Market.datesExpiry)
+    % find the boundaries for the y
+    y_min = min([Filtered_Market.strikes(ii).value, y_min]);
+    y_max = max([Filtered_Market.strikes(ii).value, y_max]);
+
+    % find the smallest step 
+    step_min = min([Filtered_Market.strikes(ii).value(2:end)-Filtered_Market.strikes(ii).value(1:end-1), step_min]);
+end
+
+y_strikes = (y_min : step_min : y_max)';
+
+x_expiries = Filtered_Market.datesExpiry';
+
+[xGrid_new, yGrid_new] = meshgrid(x_expiries, y_strikes);
+
+zGrid_new = NaN(length(y_strikes), length(x_expiries));
+
+for ii = 1 : length(x_expiries)
+    for kk = 1 : length(y_strikes)
+        for jj = 1 : length(Filtered_Market.strikes(ii).value)
+            index = max(find(Filtered_Market.strikes(ii).value == y_strikes(kk),1),0);
+            if index > 0 
+                zGrid_new(kk, ii) = Filtered_Market.OTM_ImpVol(ii).value(index);
+            else 
+                zGrid_new(kk, ii) = NaN;
+            end
+        end
+    end
+end
+% size(xGrid_new)
+% size(yGrid_new)
+zGrid_new_interplated = zGrid_new;
+for ii = 8 : length(x_expiries)
+    vector_numbers = ~isnan(zGrid_new(:, ii));
+    vector_not_numbers = isnan(zGrid_new(:, ii));
+    indexes = find(vector_numbers > 0);
+    zGrid_new_interpolated(indexes(1):indexes(end), ii) =  interp1(y_strikes(indexes), zGrid_new(indexes,ii), y_strikes(indexes(1):indexes(end)));
+end
+for ii = 1 : length(x_expiries)
+    for jj = 1 : length(y_strikes)
+          if zGrid_new_interpolated(jj, ii) == 0
+              zGrid_new_interpolated(jj, ii) = NaN;
+          end
+    end
+end
+zGrid_new_interpolated;
+zGrid_new_plot = [zGrid_new(:,1:8), zGrid_new_interpolated(:,9:end)];
+surf(xGrid_new, yGrid_new, zGrid_new_plot);
+xlabel('Expiries');
+ylabel('Strikes');
+zlabel('Implied Volatilities');
+title('3D Surf of Implied Volatilities');
+grid on;
+end 
