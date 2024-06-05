9ba422484568f6dfb7b6bf53b749f3173bcb4656
NicoToia <160607944+NicoloToia@users.noreply.github.com>
2024-06-05 13:09:06 +0200

    Update plot_fwd_prices.m

--- Matlab_Code/Forward_Discounts/plot_fwd_prices.m
+++ Matlab_Code/Forward_Discounts/plot_fwd_prices.m
@@ -1,4 +1,4 @@
-function plot_fwd_prices(Market)
+function plot_fwd_prices(Market, title_mkt)
 % This function plots forward price with respect to strikes for each maturity
 % that appears in the market data struct (mid, Bid, Ask)
 %
@@ -8,25 +8,31 @@ function plot_fwd_prices(Market)
 
 % We loop over the maturities
 for ii = 1:length(Market.datesExpiry)
-    % Inizialize the figure
+    % Initialize the figure
     figure;
     
     % We plot the forward prices (mid, Bid, Ask) for each maturity
-    plot(Market.strikes(ii).value, Market.F(ii).value, 'LineWidth', 2, 'Marker', 'o');
+    plot(Market.strikes(ii).value, Market.F(ii).value, 'g', 'LineWidth', 1);
     hold on;
-    plot(Market.strikes(ii).value, Market.FBid(ii).value, 'LineWidth', 2, 'Marker', 'x');
-    plot(Market.strikes(ii).value, Market.FAsk(ii).value, 'LineWidth', 2, 'Marker', 's');
+    plot(Market.strikes(ii).value, Market.FBid(ii).value, 'b', 'LineWidth', 1.5, 'Marker', 'x');
+    plot(Market.strikes(ii).value, Market.FAsk(ii).value, 'r', 'LineWidth', 1.5, 'Marker', 'x');
+    
+    % Set horizontal lines for the minimum of the ask prices and the maximum of the bid prices
+    yline(min(Market.FAsk(ii).value), 'r--', 'LineWidth', 1);
+    yline(max(Market.FBid(ii).value), 'b--', 'LineWidth', 1);
     
     % Figure improvements
-    title(['Forward Prices w.r.t. Strikes for Expiry Date: ', datestr(Market.datesExpiry(ii))]);
+    title(['Forward Prices w.r.t. Strikes for Expiry Date: ', datestr(Market.datesExpiry(ii)), ' - ', title_mkt]);
     xlabel('Strikes');
     ylabel('Fwd Prices');
     grid on;
-    legend({'F', 'FBid', 'FAsk'}, 'Location', 'Best');
-    set(gca, 'FontSize', 14);
-    set(gcf, 'Position', [100, 100, 800, 600]); 
+    legend({'F', 'FBid', 'FAsk', 'Min Ask', 'Max Bid'}, 'Location', 'Best');
+    set(gca, 'FontSize', 10);
+    % set(gcf, 'Position', [100, 100, 800, 600]); 
     hold off;
-
 end
 
+
+
 end
+    
