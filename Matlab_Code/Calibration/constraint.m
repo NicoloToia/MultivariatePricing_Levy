b698aafabcc80bb8497f54817e7fbd6fa1a7aebb
NicoToia <160607944+NicoloToia@users.noreply.github.com>
2024-06-05 13:08:14 +0200

    Update constraint.m

--- Matlab_Code/Calibration/constraint.m
+++ Matlab_Code/Calibration/constraint.m
@@ -15,10 +15,10 @@ function [c, ceq] = constraint(x, alpha)
 
 % Inequality constraint
 
+c = [];
+
 % c = [ - (1-alpha) / (x(2)*x(1)^2) - x(3);
 %       - (1-alpha) / (x(5)*x(4)^2) - x(6) ];
-
-c = [];
 
 % Equality constraint
 ceq = x(1)^2/(x(3)^2*x(2)) - x(4)^2/(x(6)^2*x(5));
